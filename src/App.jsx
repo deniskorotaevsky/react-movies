58e5c3bb5b4d71d797e94e43600d714fcf927304
Denis Korotaevsky <huligan8771@gmail.com>
2022-07-18 02:42:34 +0300

    header,main,footer

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Footer } from "./Layout/Footer";
+import { Header } from "./Layout/Header";
+import { Main } from "./Layout/Main";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+
+  );
+
+}
+
+export default App;
---
+++ src/Layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="#80cbc4 teal lighten-3">
+        <div classNameName="nav-wrapper">
+            <a href="#" className="brand-logo">React Movies</a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="#">Repo</a></li>
+            </ul>
+        </div>
+    </nav>
+}
+
+export { Header }
